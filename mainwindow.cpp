6c9eb6232f2b3f7897e8220ce84ba91759316e41
izformatika <izformatika@gmail.com>
2020-04-23 22:18:39 +0300

    resized main window, added a button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,11 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->resize(350,450);
+    reset=new QPushButton(this);
+    reset->setText("Restart");
+    reset->resize(150,50);
+    reset->move(50,50);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QPushButton * reset;
 };
 #endif // MAINWINDOW_H
